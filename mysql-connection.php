2fb26156ca1d77568c15dcc735a0f4ed5e862883
adarsh62656 <55345202+adarsh62656@users.noreply.github.com>
2022-04-05 15:13:55 +0530

    Update mysql-connection.php

--- mysql-connection.php
+++ mysql-connection.php
@@ -2,10 +2,13 @@
 <html>
 <body>
 <?php
-echo "Trying to Connect SQL Server <br>";
-$link = mysqli_connect("localhost","root","Adarsh@1234");
+$endpoint=strval(file_get_contents("/var/www/html/cred/db_end_point"));
+$username=strval(file_get_contents("/var/www/html/cred/db_username"));
+$password=strval(file_get_contents("/var/www/html/cred/db_password"));
+$temp="terraform-20220404100816620400000001.cq174vfsoqja.us-east-1.rds.amazonaws.com";
+$link = mysqli_connect($temp,"admin","adarsh1234");
 if (!$link){
-	die('Could not Connect: '.mysqli_error());
+        die('Could not Connect: '.mysqli_error());
 }
 echo "Connected Successfully";
 mysqli_close($link);
