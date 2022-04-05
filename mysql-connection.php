131a67c497d16e9a969b675daf3cd8ed61cd9b0e
adarsh62656 <55345202+adarsh62656@users.noreply.github.com>
2022-04-05 15:19:41 +0530

    Update mysql-connection.php

--- mysql-connection.php
+++ mysql-connection.php
@@ -5,7 +5,7 @@
 $endpoint=strval(file_get_contents("/var/www/html/cred/db_end_point"));
 $username=strval(file_get_contents("/var/www/html/cred/db_username"));
 $password=strval(file_get_contents("/var/www/html/cred/db_password"));
-$link = mysqli_connect($temp,"admin","adarsh1234");
+$link = mysqli_connect($temp,$username,$password);
 if (!$link){
         die('Could not Connect: '.mysqli_error());
 }
