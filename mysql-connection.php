779813d4fd2ab54abb527b1d143cc20673963b18
adarsh62656 <55345202+adarsh62656@users.noreply.github.com>
2022-04-05 15:18:54 +0530

    Update mysql-connection.php

--- mysql-connection.php
+++ mysql-connection.php
@@ -5,7 +5,6 @@
 $endpoint=strval(file_get_contents("/var/www/html/cred/db_end_point"));
 $username=strval(file_get_contents("/var/www/html/cred/db_username"));
 $password=strval(file_get_contents("/var/www/html/cred/db_password"));
-$temp="terraform-20220404100816620400000001.cq174vfsoqja.us-east-1.rds.amazonaws.com";
 $link = mysqli_connect($temp,"admin","adarsh1234");
 if (!$link){
         die('Could not Connect: '.mysqli_error());
